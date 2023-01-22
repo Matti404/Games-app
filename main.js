810e52d77314a69eaff57d68ca081ad3e12438e6
matti404 <maciej.andraka.ma@gmail.com>
2023-01-22 12:57:50 +0100

    change style to nav, prevent duplicate items added to shop, adapt alert window to changes

--- main.js
+++ main.js
@@ -17,9 +17,222 @@ const hamburger = document.getElementById("hamburger");
 const navBar = document.querySelector(".info");
 const moreGameBtn = document.getElementById("more");
 
-// Create div games
 
-function createGameGrid() {
+// promise
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4899ba6d80msh8dfc66190a08b9bp1eca1bjsn47644093a240",
+    "X-RapidAPI-Host": "cheapshark-game-deals.p.rapidapi.com",
+  },
+};
+
+let finalData = "";
+
+async function loadData() {
+  const res = await fetch(
+    "https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "4899ba6d80msh8dfc66190a08b9bp1eca1bjsn47644093a240",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    }
+    );
+    let data = await res.json();
+    
+    return data;
+  }
+  const shop = document.querySelector(".shop");
+  const addToCartButtons = document.querySelectorAll(".game__btn");
+  const buyButtons = document.querySelectorAll(".list__buy");
+  const ulList = document.querySelector(".list");
+  const liItem = document.querySelector(".list__item");
+  const deleteBtn = document.querySelector(".list__delete");
+  
+  let i = 0;
+  
+  async function showGames() {
+    let allGames = await loadData();
+    console.log(allGames);
+    allGames = allGames.slice(0, 3);
+    cartNumber.classList.add("hide-number");
+    allGames.forEach((values) => {
+      const item = document.createElement("div");
+      item.classList.add("game__div");
+      item.innerHTML = `
+      <img src=${values.thumbnail} height="200" width="156"/>
+      <h5 class="game__name">${values.title}</h5>
+      <button class="game__btn" onclick='addToCart(${values.id})'>Add to cart</button>
+      </div>`;
+      gameContainer.appendChild(item);
+    });
+  }
+  
+  function clickedGame() {
+    if (cartNumber.innerHTML == 1) {
+      cartNumber.classList.remove("hide-number");
+    }
+  }
+  
+  let cart = [];
+  
+  async function addToCart(id) {
+    let allGames = await loadData();
+    
+    if (cart.some((item) => item.id === id)) {
+      alert("Game is already in shop list");
+    } else {
+      const item = allGames.find((product) => product.id === id);
+      cart.push(item);
+      console.log(cart);
+      
+      // Add game li tag to shoping list
+      const addedGameListItem = document.createElement("li");
+      addedGameListItem.classList.add("list__item");
+      const addedGameImage = document.createElement("img");
+      
+      cart.forEach((id) => {
+        addedGameImage.setAttribute("src", id.thumbnail);
+        addedGameImage.setAttribute("height", 70);
+        addedGameImage.setAttribute("width", 70);
+        addedGameListItem.appendChild(addedGameImage);
+      });
+
+      const addedGameButton = document.createElement("button");
+      addedGameButton.classList.add("list__buy");
+      addedGameButton.innerText = "Buy now";
+      const deleteBtn = document.createElement("button");
+      deleteBtn.classList.add("list__delete");
+      deleteBtn.innerText = "X";
+      addedGameListItem.appendChild(addedGameButton);
+      addedGameListItem.appendChild(deleteBtn);
+      console.log(addedGameListItem);
+      ulList.appendChild(addedGameListItem);
+      ulList.classList.add("hide");
+      
+      // Delete games in shoping list
+      
+      deleteBtn.addEventListener("click", (e) => {
+        if (e.target && e.target.classList == "list__delete") {
+          cartNumber.innerHTML = --i;
+          ulList.removeChild(addedGameListItem);
+          cart.splice(e, 1)
+          if (cartNumber.innerHTML == 0) {
+          cartNumber.classList.add("hide-number");
+        }
+      }
+    });
+    cartNumber.innerHTML = ++i;
+  }
+  clickedGame();
+
+  
+}
+
+showGames();
+
+let start = 3;
+let end = 5;
+
+async function sliced() {
+  let currentGames = await loadData();
+  console.log(currentGames);
+  
+  currentGames = currentGames.slice(start, end);
+  start += 2;
+  end += 2;
+  currentGames.forEach((values) => {
+    const item = document.createElement("div");
+    item.classList.add("game__div");
+    item.innerHTML = `
+    <img src=${values.thumbnail} height="200" width="156"/>
+    <h5 class="game__name">${values.title}</h5>
+    <button class="game__btn" onclick='addToCart(${values.id})'>Add to cart</button>
+    </div>`;
+    gameContainer.appendChild(item);
+  });
+}
+
+async function searchLetters(e) {
+  gameContainer.innerHTML = "";
+  const allGames = await loadData();
+  
+  if (e.target.value.length === 0) {
+    showGames();
+  } else {
+    for (let j = 0; j < allGames.length; j++) {
+      if (
+        e.target.value == allGames[j].title ||
+        e.target.value == allGames[j].title.toLowerCase()
+        ) {
+          let filterGame = document.createElement("div");
+          filterGame.classList.add("game__div");
+          let filterImg = document.createElement("img");
+          filterImg.setAttribute("src", allGames[j].thumbnail);
+          filterImg.setAttribute("height", 200);
+          filterImg.setAttribute("width", 156);
+          
+          let filterName = document.createElement("h5");
+          filterName.classList.add("game__name");
+          filterName.innerText = allGames[j].title;
+          
+          let filterGameBtn = document.createElement("button");
+          filterGameBtn.classList.add("add-selected-btn");
+        filterGameBtn.innerText = "Add to cart";
+        filterGameBtn.setAttribute("game", JSON.stringify(allGames[j]));
+        
+        filterGame.appendChild(filterImg);
+        filterGame.appendChild(filterName);
+        filterGame.appendChild(filterGameBtn);
+        
+        gameContainer.appendChild(filterGame);
+      }
+    }
+  }
+}
+
+// addEventListeners
+
+hamburger.addEventListener('click', () => {
+  navBar.classList.toggle('show-navbar');
+});
+
+shop.addEventListener('click', () => {
+  ulList.classList.toggle('hide');
+  ulList.classList.toggle('appear');
+});
+
+document.querySelectorAll('.img').forEach(function (el) {
+  el.addEventListener('click', function () {
+    var src = this.getAttribute('src');
+    el.src = src;
+  });
+});
+
+moreGameBtn.addEventListener("click", sliced);
+
+searchInput.addEventListener("change", searchLetters);
+
+// Alert if user has bought a game
+
+ulList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('list__buy')) {
+    alert("Congratulations, you have bought a game");
+  }
+});
+
+
+
+
+
+
+
+
+
+/*function createGameGrid() {
   for (let i = 0; i < games.length; i++) {
     let gameEl = document.createElement("div");
     gameEl.classList.add("game__div");
@@ -66,7 +279,7 @@ function addingNumber() {
   if (cartNumber.innerHTML == 1) {
     cartNumber.classList.remove('hide-number');
   } 
-}*/
+}
 //addingNumber()
 
 // Adding game to store cart
@@ -92,226 +305,10 @@ const addGameToCart = (game) => {
   //gamesAddedToCart.push(game);
   ulList.appendChild(addedGameListItem);
   //ulList.classList.add('hide');
-};
-
-// Clicking on button creates li and number
-
-/*addToCartButtons.forEach(button => {
-  
-  button.addEventListener('click', (e) => {
-    const game = JSON.parse(e.currentTarget.attributes.game.value);
-    addingNumber()
-    //addGameToCart(game);
-  });
- });*/
-
-// promise
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "4899ba6d80msh8dfc66190a08b9bp1eca1bjsn47644093a240",
-    "X-RapidAPI-Host": "cheapshark-game-deals.p.rapidapi.com",
-  },
-};
-
-let finalData = "";
-
-async function loadData() {
-  const res = await fetch(
-    "https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc",
-    {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "4899ba6d80msh8dfc66190a08b9bp1eca1bjsn47644093a240",
-        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-      },
-    }
-  );
-  let data = await res.json();
-
-  return data;
-}
-const shop = document.querySelector(".shop");
-const addToCartButtons = document.querySelectorAll(".game__btn");
-const buyButtons = document.querySelectorAll(".list__buy");
-const ulList = document.querySelector(".list");
-const liItem = document.querySelector(".list__item");
-const deleteBtn = document.querySelector(".list__delete");
-
-let i = 0;
-
-async function showGames() {
-  let allGames = await loadData();
-  console.log(allGames);
-  allGames = allGames.slice(0, 3);
-  cartNumber.classList.add("hide-number");
-  allGames.forEach((values) => {
-    const item = document.createElement("div");
-    item.classList.add("game__div");
-    item.innerHTML = `
-                <img src=${values.thumbnail} height="200" width="156"/>
-                <h5 class="game__name">${values.title}</h5>
-                <button class="game__btn" onclick='addToCart(${values.id})'>Add to cart</button>
-                </div>`;
-    gameContainer.appendChild(item);
-  });
-}
-
-function clickedGame() {
-  //let i = 0;
-  if (cartNumber.innerHTML == 1) {
-    cartNumber.classList.remove("hide-number");
-  }
-}
-
-let cart = [];
-
-async function addToCart(id) {
-  let allGames = await loadData();
-
-  if (cart.some((item) => item.id === id)) {
-    alert("Game is already in shop list");
-  } else {
-    const item = allGames.find((product) => product.id === id);
-    cart.push(item);
-    console.log(item);
-
-    // Add game li tag to shoping list
-    const addedGameListItem = document.createElement("li");
-    addedGameListItem.classList.add("list__item");
-    const addedGameImage = document.createElement("img");
-
-    cart.forEach((it) => {
-      addedGameImage.setAttribute("src", it.thumbnail);
-      addedGameImage.setAttribute("height", 70);
-      addedGameImage.setAttribute("width", 70);
-      addedGameListItem.appendChild(addedGameImage);
-    });
-
-    const addedGameButton = document.createElement("button");
-    addedGameButton.classList.add("list__buy");
-    addedGameButton.innerText = "Buy now";
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("list__delete");
-    deleteBtn.innerText = "X";
-    addedGameListItem.appendChild(addedGameButton);
-    addedGameListItem.appendChild(deleteBtn);
-    console.log(addedGameListItem);
-    ulList.appendChild(addedGameListItem);
-
-    // Delete games in shoping list
-
-    deleteBtn.addEventListener("click", (e) => {
-      if (e.target && e.target.classList == "list__delete") {
-        cartNumber.innerHTML = --i;
-        ulList.removeChild(addedGameListItem);
-        if (cartNumber.innerHTML == 0) {
-          cartNumber.classList.add("hide-number");
-        }
-      }
-    });
-  }
-  cartNumber.innerHTML = ++i;
-  clickedGame();
-}
-
-showGames();
-
-let start = 3;
-let end = 5;
-
-async function sliced() {
-  let currentGames = await loadData();
-  console.log(currentGames);
-
-  currentGames = currentGames.slice(start, end);
-  start += 2;
-  end += 2;
-  currentGames.forEach((values) => {
-    const item = document.createElement("div");
-    item.classList.add("game__div");
-    item.innerHTML = `
-                <img src=${values.thumbnail} height="200" width="156"/>
-                <h5 class="game__name">${values.title}</h5>
-                <button class="game__btn" onclick='addToCart(${values.id})'>Add to cart</button>
-                </div>`;
-    gameContainer.appendChild(item);
-  });
-}
-
-async function searchLetters(e) {
-  gameContainer.innerHTML = "";
-  const allGames = await loadData();
-
-  if (e.target.value.length === 0) {
-    showGames();
-  } else {
-    for (let j = 0; j < allGames.length; j++) {
-      if (
-        e.target.value == allGames[j].title ||
-        e.target.value == allGames[j].title.toLowerCase()
-      ) {
-        let filterGame = document.createElement("div");
-        filterGame.classList.add("game__div");
-        let filterImg = document.createElement("img");
-        filterImg.setAttribute("src", allGames[j].thumbnail);
-        filterImg.setAttribute("height", 200);
-        filterImg.setAttribute("width", 156);
-
-        let filterName = document.createElement("h5");
-        filterName.classList.add("game__name");
-        filterName.innerText = allGames[j].title;
-
-        let filterGameBtn = document.createElement("button");
-        filterGameBtn.classList.add("add-selected-btn");
-        filterGameBtn.innerText = "Add to cart";
-        filterGameBtn.setAttribute("game", JSON.stringify(allGames[j]));
-
-        filterGame.appendChild(filterImg);
-        filterGame.appendChild(filterName);
-        filterGame.appendChild(filterGameBtn);
-
-        gameContainer.appendChild(filterGame);
-      }
-    }
-  }
-}
-
-// addEventListeners
-
-hamburger.addEventListener("click", () => {
-  navBar.classList.toggle("show-navbar");
-});
-
-shop.addEventListener("click", () => {
-  //list.classList.toggle('appear');
-  ulList.classList.toggle("hide");
-  //ulList.classList.toggle('make');
-});
-
-document.querySelectorAll(".img").forEach(function (el) {
-  el.addEventListener("click", function () {
-    var src = this.getAttribute("src");
-    el.src = src;
-  });
-});
-
-moreGameBtn.addEventListener("click", sliced);
-
-searchInput.addEventListener("change", searchLetters);
-
-// Alert if user has bought a game
-
-ulList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("list__buy")) {
-    alert("Congratulations, you have bought a game");
-  }
-});
-
+}*/
 /*let filterGame = document.createElement('div');
- filterGame.classList.add('game__div');
- let filterImg = document.createElement('img');
+filterGame.classList.add('game__div');
+let filterImg = document.createElement('img');
  filterImg.setAttribute('src', values.thumbnail);
  filterImg.setAttribute('height', 200);
  filterImg.setAttribute('width', 156);
